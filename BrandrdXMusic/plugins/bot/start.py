004ec1bb529fb4f4a25c20fa97f0cb76a4c6fe7f
THUNDER <143776436+BRANDED-THUNDER@users.noreply.github.com>
2024-02-15 21:41:36 +0530

    Update start.py

--- BrandrdXMusic/plugins/bot/start.py
+++ BrandrdXMusic/plugins/bot/start.py
@@ -84,7 +84,7 @@ async def start_pm(client, message: Message, _):
     else:
         out = private_panel(_)
         await message.reply_video(
-            video="https://telegra.ph//file/5374701ae0678848e9631.mp4",
+            video="https://te.legra.ph/file/5b212c75f05913ec5dde2.mp4",
             caption=_["start_2"].format(message.from_user.mention, app.mention),
             reply_markup=InlineKeyboardMarkup(out),
         )
